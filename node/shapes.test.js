4c910299d4b528abf2f527ccdfa9a71f7c04db7c
Lalu423 <>
2023-05-24 21:07:54 -0400

    added tests

--- node/shapes.test.js
+++ node/shapes.test.js
@@ -2,10 +2,28 @@ const {Circle, Triangle, Square} = require('./shapes');
 
 describe('Circle', () => {
     test('should return true if shape is circle', () => {
-        const circle = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="90" fill="red"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="green">eee</text></svg>';
+        const circle = '<circle cx="150" cy="100" r="90" fill="undefined"/>';
         const circleEl = new Circle();
         const circleRen = circleEl.render();
         expect(circleRen).toEqual(circle);
+    })
+})
+
+describe('Triangle', () => {
+    test('should return true if shape is triangle', () => {
+        const triangle = '<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="undefined"/>';
+        const triangleEl = new Triangle();
+        const triangleRen = triangleEl.render();
+        expect(triangleRen).toEqual(triangle);
+    })
+})
+
+describe('Square', () => {
+    test('should return true if shape is square', () => {
+        const square = '<rect x="50" height="200" width="200" fill="undefined"/>';
+        const squareEl = new Square();
+        const squareRen = squareEl.render();
+        expect(squareRen).toEqual(square);
     })
 })
 
